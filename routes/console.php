3833243aefd928170c927d062a3d84131f1641f0
mcddx330 <monobeat.030+github@gmail.com>
2024-10-18 19:39:17 +0900

    fix.

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::call('run:post')->everyMinute()->when(function () {
+Schedule::command('run:post')->everyMinute()->when(function () {
     return now()->minute % 5 === 1; // ファイルの存在確実性のため、+1分遅延
 });
